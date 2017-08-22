c9590b8b792446f77c71c5ce037969804711b25f
Roque Chacon Corrales <roke12chacon@gmail.com>
2017-08-22 09:13:20 -0600

    Elimina Mensajes

--- ProyectoConBootstrap/js/functionsChat.js
+++ ProyectoConBootstrap/js/functionsChat.js
@@ -21,4 +21,58 @@ function fillData() {
         }
 
     }
+    seleccion();
+}
+
+function seleccion() {
+
+    var table = document.getElementById("tableMensajes");
+    var rows = table.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].onclick = (function () {
+            return function () {
+
+
+                var mensaje = this.cells[0].innerHTML;
+               
+
+                if (confirm("Esta seguro que quiere eliminar este mensaje?") == true) {
+                    deleteMensaje(mensaje);
+                } else {
+
+                }
+            }
+        })(i);
+    }
+}
+
+function deleteMensaje(mensaje) {
+    var mensajes = JSON.parse(localStorage.getItem('mensajes'));
+    var idUserActive = sessionStorage.getItem('useractive');
+    var listaMensajes = [];
+
+    for (var i = 0; i < mensajes.length; i++) {
+        if ((idUserActive == mensajes[i].idUsuario) && (mensaje == mensajes[i].texto)) {
+
+            alert("Eliminado exitoso.")
+
+        } else {
+            listaMensajes.push(mensajes[i]);
+        }
+
+
+
+    }
+    localStorage.setItem('mensajes', JSON.stringify(listaMensajes));
+    location.reload();
+
+
+
+
+
+
+
+
+
+
 }
